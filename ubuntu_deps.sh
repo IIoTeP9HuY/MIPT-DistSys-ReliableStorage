fd57e3e875d9d325775f007f41d6a2fcbfcc402c
Andrey Kashin <iiotep9huy@gmail.com>
2014-11-03 19:58:44 +0300

    Prettify ubuntu deps script

--- ubuntu_deps.sh
+++ ubuntu_deps.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
 
-PROJECT_DIR=/tmp/hw
-
-mkdir -p $PROJECT_DIR
-cd $PROJECT_DIR
-cp /tmp/hw.tar.bz2 $PROJECT_DIR
-tar -xf hw.tar.bz2
-
+# Install deps
 sudo apt-get update
-sudo apt-get install cmake maven libthrift-java
-sudo apt-get install libboost-dev libboost-test-dev libboost-program-options-dev libboost-system-dev libboost-filesystem-dev libevent-dev automak
-e libtool flex bison pkg-config g++ libssl-dev
-sudo apt-get install python-dev
-sudo apt-get install openjdk-7-jdk
+sudo apt-get install cmake maven libthrift-java libboost-dev libboost-test-dev libboost-program-options-dev libboost-system-dev libboost-filesystem-dev libevent-dev automak
+e libtool flex bison pkg-config g++ libssl-dev python-dev openjdk-7-jdk
 
-cd /tmp
+# Install thrift
 wget http://archive.apache.org/dist/thrift/0.9.1/thrift-0.9.1.tar.gz
 tar -xf thrift-0.9.1.tar.gz
 cd thrift-0.9.1
@@ -25,7 +16,3 @@ for i in ../*.o; do echo $i; ln -s $i .; done
 cd -
 make
 sudo make install
-
-cd $PROJECT_DIR
-./build.sh
-./run_tests.sh
