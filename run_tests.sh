68abb61b0b065a03b66b401b1767f56b4567755e
Andrey Kashin <iiotep9huy@gmail.com>
2014-11-03 18:34:39 +0300

    Fixing maven dependencies exporting.

--- run_tests.sh
+++ run_tests.sh
@@ -7,7 +7,7 @@ SERVER_PID=$!
 sleep 1
 
 echo "Running tests"
-java -cp build/java/storage-1.0-SNAPSHOT.jar:/usr/lib/libthrift-0.9.1.jar:slf4j-1.7.7/slf4j-api-1.7.7.jar:slf4j-1.7.7/slf4j-simple-1.7.7.jar mipt.distsys.storage.proxy.TestCoordinator
+java -cp build/java/storage-1.0-SNAPSHOT.jar mipt.distsys.storage.proxy.TestCoordinator
 
 echo "Killing server"
 kill $SERVER_PID
