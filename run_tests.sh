e4efcbab123b7f6734675d6177fd08f4a440a9c5
Andrey Kashin <acid@yandex-team.ru>
2014-12-07 18:47:33 +0300

    Rename Server to Coordinator

--- run_tests.sh
+++ run_tests.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-echo "Running server"
+echo "Running coordinator"
 # ./build/bin/CppServer 2> server_log &
-./build/server &
+./build/coordinator &
 SERVER_PID=$!
 sleep 1
 
 echo "Running tests"
 java -cp build/java/storage-1.0-SNAPSHOT.jar mipt.distsys.storage.proxy.TestCoordinator
 
-echo "Killing server"
+echo "Killing coordinator"
 kill $SERVER_PID
